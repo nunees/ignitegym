efbcfd131163d5c982de2e719fad82259de065bb
nunees <felipe_silva_nunes@hotmail.com>
2023-07-24 16:26:50 -0300

    add Sign In screen

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,12 +1,13 @@
-import { VStack, Image, Text, Center } from "native-base";
+import { VStack, Image, Text, Center, Heading } from "native-base";
 
 import BackgroundImg from "@assets/background.png";
 
 import LogoSvg from "@assets/logo.svg";
+import { Input } from "@components/Input";
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg={"gray.700"}>
+    <VStack flex={1} bg={"gray.700"} px={10}>
       <Image
         source={BackgroundImg}
         alt="Pessoas treinando"
@@ -19,6 +20,20 @@ export function SignIn() {
           Treine sua mente e seu corpo
         </Text>
       </Center>
+
+      <Center>
+        <Heading color={"gray.100"} fontSize="xl" mb={6} fontFamily={"heading"}>
+          Acesse sua conta
+        </Heading>
+      </Center>
+
+      <Input
+        placeholder="E-mail"
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+
+      <Input placeholder="Senha" secureTextEntry />
     </VStack>
   );
 }
