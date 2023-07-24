05630ad5c3ca434f70699ad4d2f3bf8caa782e52
nunees <felipe_silva_nunes@hotmail.com>
2023-07-24 16:58:37 -0300

    finish signout

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ import {
 import { Loading } from "@components/Loading";
 import { THEME } from "./src/theme";
 import { SignIn } from "@screens/Signin";
+import { SignUp } from "@screens/Signup";
 
 export default function App() {
   LogBox.ignoreLogs([
@@ -25,7 +26,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <SignUp /> : <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ src/screens/Signup.tsx
@@ -0,0 +1,52 @@
+import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+
+import BackgroundImg from "@assets/background.png";
+
+import LogoSvg from "@assets/logo.svg";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+export function SignUp() {
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg={"gray.700"} px={10} pb={16}>
+        <Image
+          source={BackgroundImg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
+        />
+        <Center my={24}>
+          <LogoSvg />
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e seu corpo
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading
+            color={"gray.100"}
+            fontSize="xl"
+            mb={6}
+            fontFamily={"heading"}
+          >
+            Crie sua conta
+          </Heading>
+        </Center>
+
+        <Input placeholder="Nome" autoCapitalize="none" />
+
+        <Input placeholder="E-mail" keyboardType="email-address" />
+
+        <Input placeholder="Senha" secureTextEntry />
+
+        <Button title="Criar e acesssar" />
+
+        <Button title="Voltar para o login" variant="outline" mt={24} />
+      </VStack>
+    </ScrollView>
+  );
+}
